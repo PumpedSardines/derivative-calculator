2a340b37a4bb197c370f543752edbc1b0cdc85cb
Fritiof Rusck <fritiof@rusck.se>
2024-06-04 09:25:52 +0200

    feat: change index.js

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,12 @@ import { combine } from "./combine";
 
 const compile = (input: string) => evaluate(parse(tokenize(input)));
 
-[
-  "x^2",
-  "(x+8)^5",
-  "e^(x^3)",
-  "e^(3*x) - sin(pi) * x^2",
-  "e^(sin(x)) - ln(cos(x))",
-].forEach((input) => {
-  const node = compile(input);
-  const derived = derivative(node);
-  const combined = combine(derived);
-  console.log(`Derivative of ${input} is ${combined.replace(/\s/g, "")}`);
-  console.log();
-});
+["x^2", "e^(x^2 + 2*x)", "(3 * 1 + 0) * x", "(2 + 2 * 3) * x"].forEach(
+  (input) => {
+    const node = compile(input);
+    const derived = derivative(node);
+    const combined = combine(derived);
+    console.log(`Derivative of ${input} is ${combined.replace(/\s/g, "")}`);
+    console.log();
+  },
+);
